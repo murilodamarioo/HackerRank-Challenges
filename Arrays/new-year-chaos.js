1a4a0a5dfea4d33679f640cd30ec057cfc22b7c3
Murilo Damario <murilo.gcdamario@gmail.com>
2023-08-22 13:17:56 -0300

    New Year Chaotic solution

---
+++ Arrays/new-year-chaos.js
@@ -0,0 +1,26 @@
+let prompt =  require('prompt-sync')();
+
+const minimumBribes = (q) => {
+    let chaotic = false;
+    let bribes = 0;
+
+    for (let i = 0; i < q.length; i++) {
+        if (q[i] - (i+1) > 2) { chaotic = true; }
+        for (let j = q[i] - 2; j < i; j++) {
+        if (q[j] > q[i]) { bribes++; }
+        }
+    }
+
+    if(chaotic === true) {
+        console.log("Too chaotic");
+    } else {
+        console.log(bribes);
+    }
+}
+
+let t = prompt();
+for (let i = 0; i < t; i++) {
+    const q = prompt().replace(/\s+$/g, '').split(' ').map(qTemp => parseInt(qTemp));
+
+    console.log(minimumBribes(q));
+}
