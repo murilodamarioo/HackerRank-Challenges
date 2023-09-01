02706f5f9128df01203c1248b7ea740664d9df99
Murilo Damario <murilo.gcdamario@gmail.com>
2023-08-31 22:30:22 -0300

    Sherlock and the valid string - partial solution

---
+++ String-Manipulation/sherlock-and-the-valid-string.js
@@ -0,0 +1,42 @@
+const isValid = (s) => {
+    let dict = {};
+    
+    for (let char of s) {
+        dict[char] = (dict[char] || 0) + 1;
+    }
+
+    let frequencies = Object.values(dict);
+    console.log(frequencies);
+
+    let maxFrequency = Math.max(...frequencies);
+    console.log(`maxFrequency: ${maxFrequency}\n`);
+
+    let minFrequency = Math.min(...frequencies);
+    console.log(`minFrequency: ${minFrequency}\n`);
+
+    let maxFrequencyCount = frequencies.filter(frequency => frequency === maxFrequency).length;
+    console.log(`maxFrequencyCount: ${maxFrequencyCount}`);
+
+    let minFrequencyCount = frequencies.filter(frequency => frequency === minFrequency).length;
+    console.log(`minFrequencyCount: ${minFrequencyCount}`);
+
+    if (maxFrequency === minFrequency) {
+        return 'YES';
+    } else if ((maxFrequencyCount === 1 && (maxFrequency - minFrequency === 1 || minFrequency === 1)) ||
+               (minFrequencyCount === 1 && (minFrequency === 1 || maxFrequency - minFrequency === 1))) {
+        return 'YES';
+    } else {
+        return 'NO';
+    }
+    
+}
+
+
+
+let prompt = require('prompt-sync')();
+
+const s = prompt().trim();
+
+const result = isValid(s);
+
+console.log(result);
